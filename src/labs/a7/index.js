7f7d2eae825a6c2a755156e13a30b32fe524ed20
xinyu-hou <61299181+xinyu-hou@users.noreply.github.com>
2023-03-15 12:10:28 -0400

    a7 Branch Created

---
+++ src/labs/a7/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Assignment7 = () => {
+    return(
+        <>
+            <h1>Assignment 7</h1>
+        </>
+    );
+};
+export default Assignment7;
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,11 +1,18 @@
-import Assignment6 from "./a6";
+import {Routes, Route} from "react-router";
 import Nav from "../nav";
+import Assignment6 from "./a6";
+import Assignment7 from "./a7";
 
 function Labs() {
     return(
         <div>
             <Nav/>
-            <Assignment6/>
+            <Routes>
+                <Route index
+                       element={<Assignment6/>}/>
+                <Route path="a7"
+                       element={<Assignment7/>}/>
+            </Routes>
         </div>
     );
 }
