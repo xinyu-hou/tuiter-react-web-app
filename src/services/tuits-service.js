022e6db9537fb5cbd39b29aa458a99cc007d28ac
xinyu-hou <61299181+xinyu-hou@users.noreply.github.com>
2023-03-31 22:37:25 -0400

    a9 Debug Lines Deleted

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_BASE_A9;
-console.log(API_BASE);
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
